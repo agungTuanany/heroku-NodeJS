e775693db058f09bff2da0b30f74710aac73673e
agungTuanany <agung.tuanany@gmail.com>
2018-04-28 03:57:22 +0700

    ready to deploy heroku

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000 ;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    // res.send('<h1> Hello, Express! </h1>');
+    res.render("home.hbs", {
+        pageTitle: 'Home Page',
+        wellcomeMessage: 'Wellcome to our webserver',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/project', (req, res) => {
+    res.render('project.hbs',{
+        wellcomeMessage: 'portofoloio here',
+        pageTitle: "Project Page",
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+  });
